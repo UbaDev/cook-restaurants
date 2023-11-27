1b8886c353c173ff89953983eed080e8dceab3e1
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2023-11-27 17:41:50 -0600

    Cambios en homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,33 +1,231 @@
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from "react-native";
-import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from "react-native";
+import React, {useEffect, useState} from "react";
 import { Icon } from "react-native-elements";
-import { useNavigation } from "@react-navigation/native";
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
-
-// Set the API key
-// GooglePlacesAutocomplete.setAPIKey('AIzaSyAiXx54r-yAJ4Z-rHbzmJZFl-JbwipjJwI');
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { getDoc, doc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { db } from '../utils/db';
+import { auth } from '../utils/db';
 
 export default function HomeScreen() {
   const navigation = useNavigation();
+  const [name, setName] = useState('');
 
   const goToMaps = () => {
     navigation.navigate("SearchMapStack");
   }
 
+  const goToDetail = () => {
+    navigation.navigate("ProductDetailStack");
+  }
+
+  const obtenerNombreDeUsuario = async () => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          const userDocRef = doc(db, 'users', user.uid);
+
+          const userDoc = await getDoc(userDocRef);
+
+          if (userDoc.exists()) {
+            const username = userDoc.data().username;
+            setName(username);
+          } else {
+            console.log('El documento del usuario no existe en Firestore');
+          }
+        } catch (error) {
+          console.error('Error al obtener el nombre de usuario:', error);
+        }
+      }
+    });
+  };
+
+useFocusEffect(
+  React.useCallback(() => {
+    obtenerNombreDeUsuario();
+  }, [])
+);
+
+  
 
   return (
-      <GooglePlacesAutocomplete
-        placeholder="Ingresa restaurante, bar, etc."
-        onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          console.log('Buscando datos...');
-          console.log(data, details);
+    <View style={{ flex: 1}}>
+      
+        <View style={{ backgroundColor: "white",paddingTop: 60, paddingBottom: 10,justifyContent: "flex-end", paddingHorizontal: 20, borderBottomWidth: 1, borderBottomColor: "rgba(0,0,0,0.1)"}}>
+          <Text style={{fontSize: 18, fontWeight: "bold" }}>Bienvenido {name} 👋🏻</Text>
+
+
+        </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ backgroundColor: "white", paddingHorizontal: 20, paddingTop: 20}}>
+          <Text style={{ fontSize: 15, fontWeight: "bold"}}>Categorías</Text>
+
+        </View>
+          <View style={styles.header}>
+
+         
+
+            <View style={{ alignItems: "center" }}>
+              <TouchableOpacity style={styles.containerRating}>
+                <Image source={require("../../assets/icons/5-estrellas.png")} style={styles.stars} />
+              </TouchableOpacity>
+              <Text style={{ marginTop: 10 }}>5 estrellas</Text>
+
+            </View>
+            <View style={{ alignItems: "center" }}>
+              <TouchableOpacity style={styles.containerRating}>
+                <Image source={require("../../assets/icons/4-estrellas.png")} style={styles.stars} />
+              </TouchableOpacity>
+              <Text style={{ marginTop: 10 }}>4 estrellas</Text>
+            </View>
+
+            <View style={{ alignItems: "center" }}>
+
+              <TouchableOpacity style={styles.containerRating}>
+
+                <Image source={require("../../assets/icons/3-estrellas.png")} style={styles.stars} />
+              </TouchableOpacity>
+              <Text style={{ marginTop: 10 }}>3 estrellas</Text>
+
+            </View>
+
+
+
+          </View>
+
+
+
+        <View style={{ backgroundColor: "white", borderRadius: 20, paddingHorizontal: 20, marginTop: 20, marginHorizontal: 20 }}>
+          <View style={{ paddingVertical: 30, paddingLeft: 20 }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>Mejores resturantes</Text>
+          </View>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <TouchableOpacity style={{ alignSelf: "center" }} onPress={goToDetail}>
+              <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 210, height: 130, borderRadius: 20, }} />
+              <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+              <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+              <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+                <Icon name="star" size={20} color="white" />
+                <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{ alignSelf: "center", marginHorizontal: 20 }}>
+              <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 210, height: 130, borderRadius: 20, }} />
+              <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+              <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+              <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+                <Icon name="star" size={20} color="white" />
+                <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{ alignSelf: "center", marginHorizontal: 20 }}>
+              <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 210, height: 130, borderRadius: 20, }} />
+              <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+              <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+              <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+                <Icon name="star" size={20} color="white" />
+                <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+              </View>
+            </TouchableOpacity>
+          </ScrollView>
+         
+        </View>
+
+        <View style={{ backgroundColor: "white", borderRadius: 20, marginVertical: 20, marginHorizontal: 20 }}>
+          <View style={{ paddingTop: 30, paddingLeft: 20 }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>Lista resturantes</Text>
+          </View>
+          <TouchableOpacity style={{ alignSelf: "center", marginVertical: 20 }}>
+
+            <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 300, height: 180, borderRadius: 20, }} />
+            <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+            <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+            <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+              <Icon name="star" size={20} color="white" />
+              <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ alignSelf: "center", marginVertical: 20 }}>
+
+            <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 300, height: 180, borderRadius: 20, }} />
+            <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+            <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+            <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+              <Icon name="star" size={20} color="white" />
+              <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ alignSelf: "center", marginVertical: 20 }}>
+
+            <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 300, height: 180, borderRadius: 20, }} />
+            <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+            <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+            <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+              <Icon name="star" size={20} color="white" />
+              <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ alignSelf: "center", marginVertical: 20 }}>
+
+            <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 300, height: 180, borderRadius: 20, }} />
+            <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+            <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+            <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+              <Icon name="star" size={20} color="white" />
+              <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ alignSelf: "center", marginVertical: 20 }}>
+
+            <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 300, height: 180, borderRadius: 20, }} />
+            <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+            <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+            <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+              <Icon name="star" size={20} color="white" />
+              <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ alignSelf: "center", marginVertical: 20 }}>
+
+            <Image source={require("../../assets/images/logo.jpeg")} style={{ width: 300, height: 180, borderRadius: 20, }} />
+            <Text style={{ fontSize: 18, marginTop: 10, textAlign: "left" }}>Burguer King</Text>
+            <Text style={{ fontSize: 14, marginTop: 10, textAlign: "left", color: "gray" }}>Av. pie de la cuesta</Text>
+            <View style={{ backgroundColor: "#EF9F27", borderRadius: 20, marginVertical: 10, padding: 5, width: 70, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 10 }}>
+              <Icon name="star" size={20} color="white" />
+              <Text style={{ color: "white", textAlign: "center" }}>4.5</Text>
+            </View>
+          </TouchableOpacity>
+
+          
+        </View>
+
+
+      </ScrollView>
+
+      <TouchableOpacity
+        style={styles.floatingButton}
+        onPress={() => {
+          goToMaps();
         }}
-        query={{ key: 'AIzaSyAiXx54r-yAJ4Z-rHbzmJZFl-JbwipjJwI', language: 'en', region: 'mx', types: ['restaurant'] }}
-        fetchDetails={true}
-        onFail={error => console.log(error)}
-        onNotFound={() => console.log('no results')}
-      />
+      >
+        <Icon
+          name="location-pin"
+          type="entypo"
+          color="white"
+          size={30}
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -41,4 +239,28 @@ const styles = StyleSheet.create({
     padding: 15,
     elevation: 5,
   },
+  header: {
+    backgroundColor: "#fff",
+    width: "100%",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingVertical: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+
+  },
+  containerRating: {
+    backgroundColor: "#FDEDA5",
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  stars: {
+    width: 50,
+    height: 50,
+  }
+
 });
