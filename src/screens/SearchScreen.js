4c93a117767c48f990d9cc965d67537f74ff1c37
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2023-12-03 20:25:31 -0600

    Busqueda ok

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,30 +3,61 @@ import React from 'react'
 import CustomInput from '../components/input/CutomInput'
 import Icon from 'react-native-vector-icons/Ionicons';
 import CardItem from '../components/card/CardItem';
-export default function SearchScren() {
+import { getPlaces } from '../api/apiPlace';
+
+export default function SearchScreen() {
+  const [search, setSearch] = React.useState("");
+  const [places, setPlaces] = React.useState([]);
+
+  // Esta función se ejecutará cada vez que cambie el texto en el campo de búsqueda
+  const handleSearch = (text) => {
+    setSearch(text);
+
+    const location = '20.606898, -100.394212'; // Querétaro
+    const radius = 1000;
+    const types = "restaurant";
+    const query = text; // Utiliza el nuevo texto de búsqueda
+    const region = "mx:qro";
+    const maxResults = 5;
+
+    getPlaces(location, radius, types, query, region, maxResults)
+      .then((response) => {
+        console.log("este es el response del buscador: ", response);
+        setPlaces(response.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.searchBar}>
-        <CustomInput {...{ placeholder: "Buscar resturantes" }} />
+        {/* Llama a handleSearch con el nuevo texto al cambiar */}
+        <CustomInput
+          placeholder="Buscar restaurantes"
+          value={search}
+          onChangeText={(text) => handleSearch(text)} // Maneja cambios de texto
+        />
         <TouchableOpacity style={styles.icon}>
-          <Icon name="close-circle"  size={26} color="rgba(0,0,0,0.6)" />
+          <Icon name="close-circle" size={26} color="rgba(0,0,0,0.6)" />
         </TouchableOpacity>
       </View>
       <ScrollView>
         <View style={styles.items}>
-          <CardItem /> 
+          {/* Muestra los lugares encontrados */}
+          {places.map((place, index) => (
+            <CardItem key={index} place={place} />
+          ))}
         </View>
       </ScrollView>
-      
-      
-
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: "white",
   },
   searchBar: {
@@ -34,7 +65,7 @@ const styles = StyleSheet.create({
   },
   icon: {
     position: "absolute",
-    right: 10, 
+    right: 10,
     top: 14,
 
   }, items: {
